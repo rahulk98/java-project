4d76f7cef1edf2158e7999c5521b43ff9d6e9ccf
rahulk98 <rahul.krishnan27@gmail.com>
2017-09-17 20:50:07 +0530

    Dues clas

---
+++ src/Student/Dues.java
@@ -0,0 +1,34 @@
+package Student;
+import java.util.Scanner;
+public class Dues {
+    int fine;
+    String remark;
+    public Dues(){
+        fine = 0;
+        remark = "";
+    }
+    public Dues(int fine, String remark){
+        this.fine = fine;
+        this.remark = remark;
+    }
+    public void setFine(int fine){
+        this.fine = fine;
+    }
+    public void setremark(String remark){
+        this.remark = remark;
+    }
+    public int getFine(){
+        return fine;
+    }
+    public String getremark(){
+        return remark;
+    }
+    public void addDues(){
+        System.out.println("Enter fine amount in rupees");
+        Scanner s = new Scanner(System.in);
+        fine = s.nextInt();
+        System.out.println("Enter remarks");
+        remark = s.next();
+    }
+
+}
