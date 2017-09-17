ff81a14403840f8fd49101b21fd84474999695c3
rahulk98 <rahul.krishnan27@gmail.com>
2017-09-17 20:50:34 +0530

    added classes

--- src/Student/BookClass.java
+++ src/Student/BookClass.java
@@ -1,4 +1,34 @@
 package Student;
 
+
+import java.awt.*;
+import java.util.Scanner;
+
 public class BookClass {
+    String bookName;
+    String author;
+    public BookClass(){
+        bookName = "";
+        author = "";
+    }
+    public BookClass(String bookName, String author){
+        this.bookName = bookName;
+        this.author  = author;
+    }
+    public void addBook(){
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter Book name");
+        bookName = s.next();
+        System.out.println("Enter author");
+        author = s.next();
+    }
+    public String toString(){
+        return "Book Name:" + bookName + "\nAuthor:" + author;
+    }
+    public String getBookName(){
+        return bookName;
+    }
+    public String getAuthor(){
+        return author;
+    }
 }
---
+++ src/Student/Library.java
@@ -0,0 +1,56 @@
+package Student;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class Library {
+    Dues due;
+    Date issue, dueDate;
+    int issueNo=0;
+    BookClass book;
+    public Library(){
+        due = new Dues();
+        book = new BookClass();
+        Calendar cal = Calendar.getInstance();
+        cal.set(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH)+1, cal.get(Calendar.DAY_OF_MONTH));
+        issue = cal.getTime();
+        cal.set(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH)+1, cal.get(Calendar.DAY_OF_MONTH));
+        dueDate = cal.getTime();
+    }
+    public Library(Dues due, BookClass book){
+        this.due = due;
+        this.book = book;
+        Calendar cal = Calendar.getInstance();
+        cal.set(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH)+1, cal.get(Calendar.DAY_OF_MONTH));
+        issue = cal.getTime();
+        cal.set(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH)+1, cal.get(Calendar.DAY_OF_MONTH));
+        dueDate = cal.getTime();
+    }
+    public void extendDueDate(){
+    if(issueNo<=2){
+        System.out.println("Cannot extend any longer.Please return immeadiately to avoid fine");
+    }
+    else{
+        issueNo++;
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(dueDate);
+        cal.set(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH)+1, cal.get(Calendar.DAY_OF_MONTH));
+        dueDate = cal.getTime();
+    }
+    }
+    public String toString(){
+        SimpleDateFormat dateOnly = new SimpleDateFormat("dd/MM/yyyy");
+        return book.toString() + "\nIssue date" + dateOnly.format(issue) + "\nDue date:" + dateOnly.format(dueDate) + "\nFine(if any):" + due.getFine();
+
+    }
+    public void borrowBook(){
+        BookClass b = new BookClass();
+        b.addBook();
+        this.book = b;
+    }
+    public void addFine(){
+        due.addDues();
+    }
+
+}
